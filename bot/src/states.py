04a9a3f02a2834336fb4e8aa48fd6408e258d7ad
shzFas <yuraka200486@gmail.com>
2025-11-30 17:17:01 +0100

    delete all, start new system

--- bot/src/states.py
+++
@@ -1,9 +0,0 @@
-from aiogram.fsm.state import StatesGroup, State
-
-class RequestForm(StatesGroup):
-    Phone = State()
-    Name = State()
-    Category = State()
-    City = State()
-    Description = State()
-    Confirm = State()
